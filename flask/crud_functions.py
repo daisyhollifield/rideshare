b0767d1a4003ef3f592ce52b23c925afb68e38f0
Valerie Tseng <vt1@wellesley.edu>
2022-12-15 14:04:11 -0500

    valerie's updatepost progress

--- flask/crud_functions.py
+++ flask/crud_functions.py
@@ -33,6 +33,16 @@ def updateUser(conn, username, phone_number, class_year, major, hometown):
              [phone_number, class_year, major, hometown, username])
     conn.commit()
 
+def update_post(conn, username, phone_number, class_year, major, hometown):
+    """ updates the Post table with given username, type, destination, street address, 
+    city, state, zipcode, date, time, title, seats, special_request, display_now, cost"""
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''update Post set type = %s, destination = %s,  street_address = %s, city = %s, state = %s, 
+    zipcode = %s, date = %s, time = %s, title = %s, seats = %s, special_request = %s
+    cost = %s where username = %s''',
+             [destination, stadd, city, state, zipcode, date, time, title, seats, special_request, cost, username])
+    conn.commit()
+
 def updateProfilePic(conn, their_username, filename):
     """ updates the Picfile table given a filename """
     curs = dbi.dict_cursor(conn)
